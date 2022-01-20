d994f712a4fbe8ac10b1bc46ff835d47284d8949
SergeFox02 <Fox2188@gmail.com>
2022-01-20 21:20:38 +0300

    Initial commit

---
+++ src/main/java/pro/sky/homeworkemployeespringexception/HomeWorkEmployeeSpringExceptionApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homeworkemployeespringexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkEmployeeSpringExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkEmployeeSpringExceptionApplication.class, args);
+    }
+
+}
