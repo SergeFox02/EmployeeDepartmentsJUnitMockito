aeffe0cb11c874b4e0ae7a84d61a0a19c8cc5add
SergeFox02 <Fox2188@gmail.com>
2022-02-21 00:05:51 +0300

    Сделал Домашнюю работу. Вроде все как надо, но в режиме инкогнито показывает ту же корзину..

---
+++ src/main/java/pro/sky/homeworkemployeespringexception/controller/StoreController.java
@@ -0,0 +1,39 @@
+package pro.sky.homeworkemployeespringexception.controller;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.homeworkemployeespringexception.data.Products;
+import pro.sky.homeworkemployeespringexception.service.StoreService;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/store/order")
+public class StoreController {
+
+    private final StoreService storeService;
+    private final Products products = new Products();
+
+    public StoreController(StoreService storeService) {
+        this.storeService = storeService;
+    }
+
+    @GetMapping
+    public Map<Integer, String> getProductsInStore(){
+        return products.getProducts();
+    }
+
+    @GetMapping(path = "/add")
+    public List<String> addProducts(@RequestParam List<Integer> idItems) {
+        return storeService.addProduct(idItems);
+    }
+
+    @GetMapping(path = "/get")
+    public List<String> getProductsInStoreCart() {
+        return storeService.getShoppingCart();
+    }
+}
---
+++ src/main/java/pro/sky/homeworkemployeespringexception/service/StoreService.java
@@ -0,0 +1,9 @@
+package pro.sky.homeworkemployeespringexception.service;
+
+import java.util.List;
+
+public interface StoreService {
+
+    List<String> addProduct(List<Integer> idItems);
+    List<String> getShoppingCart();
+}
